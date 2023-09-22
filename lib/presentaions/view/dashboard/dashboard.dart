cc9c2c39d4823af86145c3a30e476df7c4fe86ad
devmarcuz <otunbamarcusademola@gmail.com>
2023-09-22 20:44:40 +0100

    added settings screen

--- lib/presentaions/view/dashboard/dashboard.dart
+++ lib/presentaions/view/dashboard/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:event_app/core/extensions/string_extension.dart';
 import 'package:event_app/presentaions/controllers/dashboard_controller.dart';
 import 'package:event_app/presentaions/view/my_group/my_group_main_view.dart';
 import 'package:event_app/presentaions/view/home/home.dart';
+import 'package:event_app/presentaions/view/settings/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -53,9 +54,7 @@ class Dashboard extends ConsumerWidget {
         const SizedBox.expand(
           child: Text('Calendar'),
         ),
-        const SizedBox.expand(
-          child: Text('Settings'),
-        )
+        const SettingsView(),
       ][dashboardVM.page],
     );
   }
