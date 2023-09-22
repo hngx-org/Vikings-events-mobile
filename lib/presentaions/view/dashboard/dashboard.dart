b1a7b9ef382bbbd858c78ecf6579dcab38260dc7
Nwakanma Dominion <118551746+Immadominion@users.noreply.github.com>
2023-09-22 13:52:22 +0100

    Immadominion(Slackname): Worked on the my group main screen...

--- lib/presentaions/view/dashboard/dashboard.dart
+++ lib/presentaions/view/dashboard/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:event_app/core/extensions/string_extension.dart';
 import 'package:event_app/presentaions/controllers/dashboard_controller.dart';
 import 'package:event_app/presentaions/view/my_group/my_group_main_view.dart';
+import 'package:event_app/presentaions/view/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -45,9 +46,7 @@ class Dashboard extends ConsumerWidget {
       ),
       body: [
         //!TODO: Add the pages here
-        const SizedBox.expand(
-          child: Text('Home'),
-        ),
+        const Home(),
         const SizedBox.expand(
           child: MyGroupView(),
         ),
